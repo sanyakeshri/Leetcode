b7c71b7f0a835e6081b8e8cd975edb5a3c9df510
sanyakeshri <keshrisanya621@gmail.com>
2026-06-27 08:52:12 +0530

    BestTimeToBuy&Sell

---
+++ 4.java
@@ -0,0 +1,23 @@
+// 121. Best Time to Buy and Sell Stock
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            if (price < minPrice) {
+                minPrice = price;
+            }
+
+            int profit = price - minPrice;
+
+            if (profit > maxProfit) {
+                maxProfit = profit;
+            }
+        }
+
+        return maxProfit;
+    }
+}
