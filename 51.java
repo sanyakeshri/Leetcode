b311c43d0ca3ef248534695ce0e7b8a1dbae43f1
sanyakeshri <keshrisanya621@gmail.com>
2026-08-11 04:28:30 +0530

    Range Addition II

---
+++ 51.java
@@ -0,0 +1,15 @@
+// 598. Range Addition II
+
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        int minRow = m;
+        int minCol = n;
+
+        for (int[] op : ops) {
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+
+        return minRow * minCol;
+    }
+}
